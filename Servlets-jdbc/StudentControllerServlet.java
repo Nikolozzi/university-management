8cf3aa368230a65177fbdb2acf91fee62a7741a0
Nikoloz <nikakhitiri@mail.ru>
2019-03-04 00:45:44 +0400

    Add files via upload

---
+++ Servlets-jdbc/StudentControllerServlet.java
@@ -0,0 +1,384 @@
+package com.luv2code.web.jdbc;
+
+import javax.annotation.Resource;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet(name = "StudentControllerServlet", urlPatterns = {"/StudentControllerServlet"})
+public class StudentControllerServlet extends HttpServlet {
+
+    private StudentDbUtil studentDbUtil;
+    private SubjectDbUtil subjectDbUtil;
+    @Resource(name = "jdbc/web_student_tracker")
+    private DataSource dataSource;
+
+    @Override
+    public void init() throws ServletException { //this is called by tomcat when the servlet is first initilized
+        super.init();
+
+        try{
+            studentDbUtil = new StudentDbUtil(dataSource);
+            subjectDbUtil = new SubjectDbUtil(dataSource);
+        }
+        catch(Exception e){
+            throw new ServletException(e);
+        }
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String addSubjError = null;
+        String addStudError = null;
+        String updateStudError = null;
+        String registerStudError = null;
+        try {
+            String theCommand = request.getParameter("command");
+            switch (theCommand) {
+                case "ADD":
+                    addStudError = addStudent(request, response);
+                    break;
+                case "ADD_SUBJECT":
+                    addSubjError = addSubject(request,response);
+                    break;
+                case "UPDATE":
+                    updateStudError = updateStudent(request,response);
+                    break;
+                case "REGISTER":
+                    registerStudError = registerStudent(request,response);
+                    break;
+                default:
+                   listStudents(request, response);
+            }
+        } catch (Exception e) {
+            throw new ServletException(e);
+        }
+
+        RequestDispatcher dispatcher = null;
+
+
+        if(addSubjError != null){
+            dispatcher = request.getRequestDispatcher("/add-subject-form.jsp");
+            request.setAttribute("ADD_SUBJECT_ERROR",addSubjError);
+            dispatcher.forward(request,response);
+        }
+
+        if(addStudError != null){
+            dispatcher = request.getRequestDispatcher("/add-student-form.jsp");
+            request.setAttribute("ADD_STUDENT_ERROR",addStudError);
+            dispatcher.forward(request,response);
+        }
+
+        if(updateStudError != null){
+            dispatcher = request.getRequestDispatcher("/update-student-form.jsp");
+            request.setAttribute("UPDATE_STUDENT_ERROR", updateStudError);
+            dispatcher.forward(request,response);
+        }
+
+        if(registerStudError != null){
+            dispatcher = request.getRequestDispatcher("/register-student-form.jsp");
+            request.setAttribute("REGISTER_STUDENT_ERROR", registerStudError);
+            dispatcher.forward(request,response);
+        }
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        try {
+
+            String theCommand = request.getParameter("command");
+            if(theCommand == null){
+                theCommand = "LIST";
+            }
+
+            switch(theCommand){
+                case "LOAD_UPDATE":
+                    loadStudent(request,response,"/update-student-form.jsp");
+                    break;
+                case "LOAD_REGISTER":
+                    loadStudent(request,response,"/register-student-form.jsp");
+                    break;
+                case "LOAD_SUBJECT":
+                    loadSubject(request,response);
+                    break;
+                case "DELETE":
+                    deleteStudent(request,response);
+                    break;
+                case "DELETE_SUBJECT":
+                    deleteSubject(request,response);
+                    break;
+                case "GET_STUDENTS_PER_SUBJECT":
+                    getStudentsPerSubject(request,response);
+                    break;
+                 default:
+                    listStudents(request, response);
+
+            }
+        }
+        catch(Exception e){
+            throw new ServletException(e);
+        }
+    }
+
+    private void getStudentsPerSubject(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        String subjName = request.getParameter("subjectSelect");
+        List<Student> students_subjs = studentDbUtil.getStudentsPerSubject(subjName);
+
+        request.setAttribute("STUDENT_PER_SUBJECT",students_subjs);
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/studentsPerSubject.jsp");
+        dispatcher.forward(request,response);
+    }
+
+    private void loadSubject(HttpServletRequest request, HttpServletResponse response) throws Exception {
+
+        List<Subject> subjects = subjectDbUtil.getSubjects();
+        request.setAttribute("THE_SUBJECTS", subjects);
+
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/studentsPerSubject.jsp");
+        dispatcher.forward(request,response);
+    }
+
+
+    private String registerStudent(HttpServletRequest request, HttpServletResponse response) throws Exception {
+
+        String studentId = request.getParameter("studentId");
+        if(studentId.isEmpty())
+            return "Empty Field!";
+
+        int studId = Integer.parseInt(studentId);
+        String subjName = request.getParameter("subjectSelect");
+
+        //check if the student is already registered
+        boolean checked = studentDbUtil.checkStudentRegistered(studId,subjName);
+        if(checked)
+            return "Already Registered!";
+
+        studentDbUtil.registerStudent(studId,subjName);
+
+        response.sendRedirect(request.getContextPath() + "/StudentControllerServlet?command=LIST");
+
+        return null;
+    }
+
+
+
+    private void deleteSubject(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        String theSubjectId = request.getParameter("subjectId");
+
+        subjectDbUtil.deleteSubject(theSubjectId);
+
+        listStudents(request,response);
+    }
+
+    private void deleteStudent(HttpServletRequest request, HttpServletResponse response) throws Exception {
+
+        String theStudentId = request.getParameter("studentId");
+
+        studentDbUtil.deleteStudent(theStudentId);
+
+        listStudents(request,response);
+
+    }
+
+
+    private String updateStudent(HttpServletRequest request, HttpServletResponse response) throws Exception {
+
+        String studentId = request.getParameter("studentId");
+        if(studentId.isEmpty())
+            return "Empty Field!";
+
+        int id = Integer.parseInt(studentId);
+        String firstName = request.getParameter("firstName");
+        String lastName = request.getParameter("lastName");
+        String email = request.getParameter("email");
+        String phone = request.getParameter("phoneNum");
+
+        String checkUpdatedStudent = checkStudentUpdated(id, firstName,lastName,email,phone);
+        if(checkUpdatedStudent != null)
+            return checkUpdatedStudent;
+
+        Student theStudent = new Student(id,firstName,lastName,email,phone);
+
+        studentDbUtil.updateStudent(theStudent);
+
+        response.sendRedirect(request.getContextPath() + "/StudentControllerServlet?command=LIST");
+
+        return null;
+    }
+
+    private String checkStudentUpdated(int id, String firstName, String lastName, String email, String phone)
+    throws Exception {
+
+        if(firstName.isEmpty() || lastName.isEmpty() || email.isEmpty() || phone.isEmpty())
+            return "Empty Field!";
+
+        for (int i = 0; i < firstName.length(); i++) {
+            if(!Character.isLetter(firstName.charAt(i)))
+                return "Invalid First Name!";
+        }
+
+        //check lastName
+        for (int i = 0; i < lastName.length(); i++) {
+            if(!Character.isLetter(lastName.charAt(i)))
+                return "Invalid Last Name!";
+        }
+
+        //check phone number
+        for (int i = 0; i < phone.length(); i++) {
+            if(!Character.isDigit(phone.charAt(i)))
+                return "Invalid Phone Number!";
+        }
+
+        if(studentDbUtil.checkEmailUpdated(id,email)){
+            return "This Email already exists!";
+        }
+
+        return null;
+    }
+
+    private void loadStudent(HttpServletRequest request, HttpServletResponse response, String link) throws Exception{
+
+        //read student id from form data
+        String theStudentId = request.getParameter("studentId");
+
+        //get student from database
+        Student theStudent = studentDbUtil.getStudent(theStudentId);
+
+        if(link.equals("/register-student-form.jsp")){
+            List<Subject> subjects = subjectDbUtil.getSubjects();
+            request.setAttribute("THE_SUBJECTS", subjects);
+        }
+
+        //place student in the request attribute
+        request.setAttribute("THE_STUDENT",theStudent);
+
+        //send to update-student-form.jsp or register-student-form.jsp
+        RequestDispatcher dispatcher = request.getRequestDispatcher(link);
+
+        dispatcher.forward(request,response);
+    }
+
+    private String addStudent(HttpServletRequest request, HttpServletResponse response) throws Exception {
+
+        //read student info from form data
+        String firstName = request.getParameter("firstName");
+        String lastName = request.getParameter("lastName");
+        String email = request.getParameter("email");
+        String phone = request.getParameter("phoneNum");
+
+        String checkResult = checkStudentAdded(firstName,lastName,email,phone);
+        if(checkResult != null)
+            return checkResult;
+
+        //create a new student object
+        Student theStudent = new Student(firstName,lastName,email,phone);
+
+        //add the student to the database
+        studentDbUtil.addStudent(theStudent);
+
+        //send as REDIRECT to avoid multiple inserts on reloads
+        response.sendRedirect(request.getContextPath()+"/StudentControllerServlet?command=LIST");
+
+        return null;
+    }
+
+    private String checkStudentAdded(String firstName, String lastName, String email, String phone) throws Exception {
+
+        //check firstName
+
+        if(firstName.isEmpty() || lastName.isEmpty() || email.isEmpty() || phone.isEmpty())
+            return "Empty Field!";
+
+        for (int i = 0; i < firstName.length(); i++) {
+            if(!Character.isLetter(firstName.charAt(i)))
+                return "Invalid First Name!";
+        }
+
+        //check lastName
+        for (int i = 0; i < lastName.length(); i++) {
+            if(!Character.isLetter(lastName.charAt(i)))
+                return "Invalid Last Name!";
+        }
+
+        //check phone number
+        for (int i = 0; i < phone.length(); i++) {
+            if(!Character.isDigit(phone.charAt(i)))
+                return "Invalid Phone Number!";
+        }
+
+
+        //check if the user email already exists
+
+        if(studentDbUtil.checkEmail(email)) {
+            return "Email already exists!";
+        }
+
+        return null;
+    }
+
+
+    private String addSubject(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        String subjName = request.getParameter("subjName");
+        String creditNum = request.getParameter("creditNum");
+
+        String checked = checkSubjectAdded(subjName,creditNum);
+        if(checked != null)
+            return checked;
+
+        int creditAmount = Integer.parseInt(creditNum);
+
+        Subject subject = new Subject(subjName,creditAmount);
+
+        subjectDbUtil.addSubject(subject);
+
+        response.sendRedirect(request.getContextPath()+"/StudentControllerServlet?command=LIST");
+
+        return null;
+    }
+
+    public String checkSubjectAdded(String subject, String credit) throws Exception {
+        for (int i = 0; i <credit.length() ; i++) {
+            if(!Character.isDigit(credit.charAt(i)))
+                return "Illegal Credit!";
+        }
+
+        if(credit.isEmpty() || subject.isEmpty())
+            return "Empty Field!";
+
+
+        if(subjectDbUtil.checkSubject(subject))
+            return "Subject already exists!";
+
+        return null;
+    }
+
+    private void listStudents(HttpServletRequest request, HttpServletResponse response) throws Exception {
+
+        //get students from db util
+
+        List<Student> students = studentDbUtil.getStudents();
+        List<Subject> subjects = subjectDbUtil.getSubjects();
+
+
+        //count total number of subjects and students
+        int totalStudents = studentDbUtil.countStudents();
+        int totalSubjects = subjectDbUtil.countSubjects();
+        //add students to the request
+
+        request.setAttribute("STUDENT_LIST", students);
+        request.setAttribute("SUBJECT_LIST", subjects);
+        request.setAttribute("COUNT_STUDENT",totalStudents);
+        request.setAttribute("COUNT_SUBJECT", totalSubjects);
+        //send to JSP(view)
+
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/list-students.jsp");
+
+
+        dispatcher.forward(request,response);
+    }
+}
---
+++ Servlets-jdbc/StudentDbUtil.java
@@ -0,0 +1,368 @@
+package com.luv2code.web.jdbc;
+
+import javax.sql.DataSource;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class StudentDbUtil {
+
+    private DataSource dataSource;
+
+    public StudentDbUtil(DataSource theDataSource){
+        this.dataSource = theDataSource;
+    }
+
+    public List<Student> getStudents() throws Exception {
+        List<Student> students = new ArrayList<>();
+
+        Connection con = null;
+        PreparedStatement stm = null;
+        ResultSet result = null;
+
+        try {
+            //get a connection
+            con = dataSource.getConnection();
+
+            //create sql statement
+            String sql = "SELECT * FROM student ORDER BY last_name";
+            stm = con.prepareStatement(sql);
+
+            //execute the query
+            result = stm.executeQuery();
+
+            //result set
+            while(result.next()){
+                //retrieve data from result set row
+                 int id = result.getInt("id");
+                 String firstName = result.getString("first_name");
+                 String lastName = result.getString("last_name");
+                 String email = result.getString("email");
+                 String phoneNum = result.getString("phone_number");
+
+                //create new student object
+                 Student tempStudent = new Student(id,firstName,lastName,email,phoneNum);
+
+                //add it to the list
+                students.add(tempStudent);
+            }
+
+            return students;
+        }
+        finally {
+            //close JDBC objects
+
+            close(con,stm,result);
+
+        }
+    }
+
+    private void close(Connection con, Statement stm, ResultSet result) {
+        try{
+            if(result != null){
+                result.close();
+            }
+
+            if(stm != null){
+                stm.close();
+            }
+
+            if(con != null){
+                con.close(); //puts con object back in connection pool
+            }
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public void addStudent(Student theStudent) throws Exception {
+        Connection con = null;
+        PreparedStatement statement = null;
+
+        try {
+            //get db connection
+            con = dataSource.getConnection();
+
+            //create sql for insert
+            String sql = "INSERT INTO student " +
+                    "(first_name, last_name, email, phone_number) " + "VALUES(?,?,?,?)";
+
+            statement = con.prepareStatement(sql);
+
+            //set the param values
+            statement.setString(1,theStudent.getFirstName());
+            statement.setString(2,theStudent.getLastName());
+            statement.setString(3,theStudent.getEmail());
+            statement.setString(4,theStudent.getPhoneNum());
+
+            //execute sql
+            statement.execute();
+        }
+        finally {
+            close(con,statement,null);
+
+        }
+    }
+
+    public Student getStudent(String theStudentId) throws Exception {
+        Student theStudent = null;
+        Connection con = null;
+        PreparedStatement statement = null;
+        ResultSet result = null;
+        int studentId;
+
+        try{
+
+            studentId = Integer.parseInt(theStudentId);
+
+            con = dataSource.getConnection();
+            String sql = "SELECT * FROM student WHERE id=?";
+            statement = con.prepareStatement(sql);
+            //set params for prepared statement
+            statement.setInt(1,studentId);
+
+            result = statement.executeQuery();
+
+            if(result.next()){
+                String firstName = result.getString("first_name");
+                String lastName = result.getString("last_name");
+                String email = result.getString("email");
+                String phoneNum = result.getString("phone_number");
+
+                theStudent = new Student(studentId,firstName,lastName,email,phoneNum);
+
+            }
+            else{
+                throw new Exception("Could not find student id: " + studentId);
+            }
+
+            return theStudent;
+        }
+        finally{
+            close(con,statement,result);
+        }
+    }
+
+    public void updateStudent(Student theStudent) throws Exception{
+        Connection con = null;
+        PreparedStatement statement = null;
+
+        try {
+
+            con = dataSource.getConnection();
+
+            String sql = "UPDATE student " +
+                    "SET first_name=?, last_name=?, email=?, phone_number=? "
+                    + "WHERE id=?";
+
+            statement = con.prepareStatement(sql);
+
+            statement.setString(1, theStudent.getFirstName());
+            statement.setString(2, theStudent.getLastName());
+            statement.setString(3, theStudent.getEmail());
+            statement.setString(4, theStudent.getPhoneNum());
+            statement.setInt(5, theStudent.getId());
+
+            statement.execute();
+        }
+        finally {
+            close(con,statement,null);
+        }
+    }
+
+    public void deleteStudent(String theStudentId) throws Exception {
+        Connection con = null;
+        PreparedStatement statement = null;
+
+        try{
+
+            int studentId = Integer.parseInt(theStudentId);
+
+            con = dataSource.getConnection();
+            String sql = "DELETE FROM student WHERE id=?";
+
+            statement = con.prepareStatement(sql);
+            statement.setInt(1,studentId);
+
+            statement.execute();
+
+        }
+        finally {
+            close(con,statement,null);
+        }
+
+    }
+
+    public boolean checkEmail(String email) throws Exception {
+        Connection con = null;
+        PreparedStatement statement = null;
+        ResultSet result = null;
+
+        try{
+            con = dataSource.getConnection();
+            String sql = "SELECT id FROM student WHERE email=?";
+            statement = con.prepareStatement(sql);
+            statement.setString(1,email);
+            result = statement.executeQuery();
+
+            return result.next();
+        }
+        finally {
+            close(con,statement,result);
+        }
+    }
+
+    public boolean checkEmailUpdated(int id, String email) throws Exception {
+
+        Connection con = null;
+        PreparedStatement statement = null;
+        ResultSet result = null;
+
+        try{
+
+            con = dataSource.getConnection();
+            String sql = "SELECT id FROM student WHERE email=? AND id<>?";
+            statement = con.prepareStatement(sql);
+            statement.setString(1,email);
+            statement.setInt(2,id);
+            result = statement.executeQuery();
+
+            return result.next();
+        }
+        finally {
+            close(con,statement,result);
+        }
+    }
+
+    public void registerStudent(int studId, String subjName) throws Exception {
+
+        SubjectDbUtil subjectDbUtil = new SubjectDbUtil();
+        //get subject Id by subjName
+        int subjId = getSubjID(subjName);
+
+        Connection con = null;
+        PreparedStatement statement = null;
+        ResultSet result = null;
+
+        try{
+            con = dataSource.getConnection();
+            String sql = "INSERT INTO student_subject " +
+                    "(student_id,subject_id) " + "VALUES(?,?)";
+
+            statement = con.prepareStatement(sql);
+            statement.setInt(1,studId);
+            statement.setInt(2,subjId);
+
+            statement.execute();
+        }
+        finally {
+            close(con,statement,result);
+        }
+    }
+
+    private int getSubjID(String subjName) throws Exception {
+        Connection con = null;
+        PreparedStatement statement = null;
+        ResultSet result = null;
+
+        try{
+            con = dataSource.getConnection();
+            String sql = "SELECT subject_id FROM subject WHERE subject_name=?";
+
+            statement = con.prepareStatement(sql);
+            statement.setString(1,subjName);
+
+            result = statement.executeQuery();
+
+            //System.out.println(subjId);
+            int subjId = -1;
+            if(result.next()){
+                subjId = result.getInt("subject_id");
+            }
+
+            return subjId;
+        }
+        finally {
+            close(con,statement,result);
+        }
+    }
+
+    public boolean checkStudentRegistered(int studId, String subjName) throws Exception {
+        int subjId = getSubjID(subjName);
+
+        Connection con = null;
+        PreparedStatement statement = null;
+        ResultSet result = null;
+
+        try{
+            con = dataSource.getConnection();
+            String sql = "SELECT * FROM student_subject WHERE student_id=? AND subject_id=?";
+            statement = con.prepareStatement(sql);
+            statement.setInt(1,studId);
+            statement.setInt(2,subjId);
+
+            result = statement.executeQuery();
+            return result.next();
+        }
+        finally {
+            close(con,statement,result);
+        }
+    }
+
+    public int countStudents() throws Exception {
+        Connection con = null;
+        PreparedStatement statement = null;
+        ResultSet result = null;
+
+        try{
+            con = dataSource.getConnection();
+            String sql = "SELECT COUNT(*) AS totalStud FROM student";
+            statement = con.prepareStatement(sql);
+            result = statement.executeQuery();
+
+            int count = -1;
+            if(result.next())
+                count = Integer.parseInt(result.getString("totalStud"));
+
+            return count;
+        }
+        finally {
+            close(con,statement,result);
+        }
+    }
+
+    public List<Student> getStudentsPerSubject(String subjName) throws Exception {
+        int subjId = getSubjID(subjName);
+        List<Student> theStudents = new ArrayList<>();
+        Connection con = null;
+        PreparedStatement statement = null;
+        ResultSet result = null;
+
+        try{
+            con = dataSource.getConnection();
+            String sql = "SELECT first_name, last_name, email, phone_number " +
+                    "FROM student INNER JOIN student_subject ON student.id=student_subject.student_id " +
+                    "WHERE subject_id=?";
+
+            statement = con.prepareStatement(sql);
+            statement.setInt(1,subjId);
+            result = statement.executeQuery();
+
+            while(result.next()){
+                String firstName = result.getString("first_name");
+                String lastName = result.getString("last_name");
+                String email = result.getString("email");
+                String phoneNumber = result.getString("phone_number");
+
+                Student student = new Student(firstName,lastName,email,phoneNumber);
+                theStudents.add(student);
+            }
+
+            return theStudents;
+        }
+        finally {
+            close(con,statement,result);
+        }
+    }
+}
